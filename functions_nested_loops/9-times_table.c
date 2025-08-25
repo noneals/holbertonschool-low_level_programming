f4bd4d119f6c53ce9f88b3b6164a0d38e7e776c1
noneals <none.als012@gmail.comcom>
2025-08-25 09:06:42 +0000

    Fix indentation for times_table to follow Betty style

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,32 +1,32 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0
+ * times_table - prints the 9 times table
  */
 void times_table(void)
 {
-    int i, j, prod;
+	int i, j, prod;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            prod = i * j;
-            if (j != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-                if (prod < 10)
-                    _putchar(' ');
-            }
-            if (prod >= 10)
-            {
-                _putchar((prod / 10) + '0');
-                _putchar((prod % 10) + '0');
-            }
-            else
-                _putchar(prod + '0');
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			prod = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (prod < 10)
+					_putchar(' ');
+			}
+			if (prod >= 10)
+			{
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			else
+				_putchar(prod + '0');
+		}
+		_putchar('\n');
+	}
 }
