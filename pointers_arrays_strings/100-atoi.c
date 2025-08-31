87d55f1fe11d58316c6a57dd6b037ee3a5298eeb
noneals <none.als012@hmail.com>
2025-08-31 18:35:27 +0000

    Implement _atoi function

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,33 @@
 #include "main.h"
 
 /**
- * _atoi - convert string to integer
- * @s: string to convert
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: integer representation of string
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-    int i = 0, sign = 1, num = 0;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int started = 0;
 
-    while (s[i] && (s[i] < '0' || s[i] > '9'))
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        else if (s[i] == '+')
-        i++;
-    }
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] == '+')
+			; /* ignore '+' */
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (started)
+			break;
+		i++;
+	}
 
-    while (s[i] && s[i] >= '0' && s[i] <= '9')
-    {
-        num = num * 10 + (s[i] - '0');
-        i++;
-    }
-
-    return num * sign;
+	return (result * sign);
 }
