ccb264e6c14909e810cf2310cb1705652b5efc50
noneals <none.als012@hmail.com>
2025-08-31 18:55:49 +0000

    add

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,39 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the converted integer
+ */
 int _atoi(char *s)
 {
-    int i = 0, sign = 1, result = 0, num_started = 0;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
 
-    // Skip non-digit characters, count signs
-    while (s[i] != '\0') {
-        if (s[i] == '-') sign *= -1;
-        else if (s[i] == '+'); // Multiple + cancels out, sign unchanged
-        else if (s[i] >= '0' && s[i] <= '9') {
-            num_started = 1;
-            break;
-        }
-        i++;
-    }
+	/* Skip non-digit characters and check signs */
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] == '+')
+			;
+		i++;
+	}
 
-    // If no digits found, return 0
-    if (!num_started)
-        return 0;
+	/* Build number as negative to avoid overflow */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		int digit = s[i] - '0';
 
-    // Convert digits
-    while (s[i] >= '0' && s[i] <= '9') {
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
+		result = result * 10 - digit; /* keep result negative */
+		i++;
+	}
 
-    return sign * result;
+	/* Flip result if sign is positive */
+	if (sign == 1)
+		return (-result);
+
+	return (result);
 }
