44568e3c5baf66b64e6cff772a20fdea2b9f0537
noneals <none.als012@hmail.com>
2025-08-31 20:27:19 +0000

    Finalize _atoi function: handle INT_MIN/INT_MAX safely, Betty compliant

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - Converts a string to an integer
@@ -20,12 +21,25 @@ int _atoi(char *s)
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			num_started = 1;
-			result = result * 10 + (s[i] - '0');
+
+			if (sign == -1)
+			{
+				if (result < (INT_MIN + (s[i] - '0')) / 10)
+					return (INT_MIN);
+				result = result * 10 - (s[i] - '0');
+			}
+			else
+			{
+				if (result > (INT_MAX - (s[i] - '0')) / 10)
+					return (INT_MAX);
+				result = result * 10 + (s[i] - '0');
+			}
 		}
 		else if (num_started)
 			break;
+
 		i++;
 	}
 
-	return (sign * result);
+	return (result);
 }
