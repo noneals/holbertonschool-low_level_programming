9f604b9f385b40237ab0684fbecb464475345da7
noneals <none.als012@hmail.com>
2025-08-31 20:13:55 +0000

    Fix _atoi function to be Betty-compliant

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,41 +1,31 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: the string to convert
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
  *
- * Return: the converted integer
+ * Return: The converted integer, or 0 if no numbers found
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
+	int i = 0, sign = 1, num_started = 0;
 	int result = 0;
 
-	/* Skip non-digit characters and handle signs */
-	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] == '+')
+			;
+		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			/* do nothing */
+			num_started = 1;
+			result = result * 10 + (s[i] - '0');
 		}
+		else if (num_started)
+			break;
 		i++;
 	}
 
-	/* Build number as negative to safely handle INT_MIN */
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		int digit = s[i] - '0';
-
-		result = result * 10 - digit; /* keep negative to avoid overflow */
-		i++;
-	}
-
-	/* If sign is positive, flip result */
-	if (sign == 1)
-		return (-result);
-
-	return (result);
+	return (sign * result);
 }
