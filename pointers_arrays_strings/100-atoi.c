6fec3b79e127204053a731661acd7ced395e8f4a
noneals <none.als012@hmail.com>
2025-08-31 16:25:21 +0000

    Your descriptive commit message here

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,20 +8,21 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, num = 0;
+    int i = 0, sign = 1, num = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			num = num * 10 + (s[i] - '0');
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-		}
-		i++;
-	}
+    while (s[i] && (s[i] < '0' || s[i] > '9'))
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        else if (s[i] == '+')
+        i++;
+    }
 
-	return (num * sign);
+    while (s[i] && s[i] >= '0' && s[i] <= '9')
+    {
+        num = num * 10 + (s[i] - '0');
+        i++;
+    }
+
+    return num * sign;
 }
