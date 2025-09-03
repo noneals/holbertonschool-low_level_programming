7a79f9a198fdef19a3e78ff2c82954ea50cea073
noneals <noneals012@gmail.com>
2025-09-03 14:19:21 +0000

    add 3-strcmp.c and main.h

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,28 +1,23 @@
-`#include "main.h"
-
 /**
  * _strncpy - copies a string
- * @dest: buffer storing the string copy
- * @src: source string
+ * @dest: buffer where we copy the string
+ * @src: the source string
  * @n: maximum number of bytes to copy
- *
- * Description: Custom implementation of the strncpy behavior
- *              from the C standard library.
  *
  * Return: pointer to dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	for (; i < n; i++)
-	{
-		dest[i] = '\0';
-	}
-
-	return (dest);
+int i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
 }
