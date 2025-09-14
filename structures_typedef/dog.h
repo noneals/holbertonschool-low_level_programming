2240748481c98497c11daf877aaba2ca1850f799
noneals <none.als012@gmail.com>
2025-09-14 14:56:08 +0000

    update dog file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,12 +6,15 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ *
+ * Description: This structure stores information about a dog, 
+ * including its name, age, and owner's name.
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif /* DOG_H */
