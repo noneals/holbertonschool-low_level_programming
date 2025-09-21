f9abc207c23b1664cf307b8af585d12f99cb42bd
noneals <none.als012@gmail.com>
2025-09-21 17:28:45 +0000

    Add 0-print_name, 1-array_iterator, 2-int_index, 3-calc files with function pointers

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer in an array
+ * @array: array of integers
+ * @size: number of elements in the array
+ * @cmp: pointer to function used to compare values
+ *
+ * Return: index of the first element for which cmp does not return 0,
+ *         -1 if no element matches or size <= 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+
+	return (-1);
+}
---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stddef.h>
+
+/**
+ * get_op_func - selects the correct operation function
+ * @s: operator as string
+ *
+ * Return: pointer to corresponding function, or NULL if not found
+ */
+int (*get_op_func(char *s))(int, int)
+{
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i = 0;
+
+    while (ops[i].op && ops[i].op[0] != s[0])
+        i++;
+
+    return (ops[i].op ? ops[i].f : NULL);
+}
