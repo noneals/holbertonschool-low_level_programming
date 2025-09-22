208be3bb3fe7462e4e9a894714706f372cd3efe3
noneals <124405266+noneals@users.noreply.github.com>
2025-09-22 15:35:45 +0300

    Update 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -12,31 +12,33 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
-	char *sep = "";
-	char *str;
+	char *str, *sep = "";
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%s%c", sep, va_arg(args, int));
+			break;
+		case 'i':
+			printf("%s%d", sep, va_arg(args, int));
+			break;
+		case 'f':
+			printf("%s%f", sep, va_arg(args, double));
+			break;
+		case 's':
+			str = va_arg(args, char *);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s%s", sep, str);
+			break;
+		}
 		if (format[i] == 'c' || format[i] == 'i' ||
 		    format[i] == 'f' || format[i] == 's')
-		{
-			if (format[i] == 'c')
-				printf("%s%c", sep, va_arg(args, int));
-			if (format[i] == 'i')
-				printf("%s%d", sep, va_arg(args, int));
-			if (format[i] == 'f')
-				printf("%s%f", sep, va_arg(args, double));
-			if (format[i] == 's')
-			{
-				str = va_arg(args, char *);
-				if (!str)
-					str = "(nil)";
-				printf("%s%s", sep, str);
-			}
 			sep = ", ";
-		}
 		i++;
 	}
 
