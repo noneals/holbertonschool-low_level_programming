37f1bdcc81f0400b3382d5f332aa2ee68ba2410a
noneals <124405266+noneals@users.noreply.github.com>
2025-10-19 16:06:13 +0300

    Update betty 0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,28 +9,28 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table;
-    unsigned long int i;
+	hash_table_t *table;
+	unsigned long int i;
 
-    /* Allocate memory for the hash table structure */
-    table = malloc(sizeof(hash_table_t));
-    if (table == NULL)
-        return (NULL);
+	/* Allocate memory for the hash table structure */
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
 
-    /* Set size */
-    table->size = size;
+	/* Set size */
+	table->size = size;
 
-    /* Allocate memory for the array of pointers */
-    table->array = malloc(sizeof(hash_node_t *) * size);
-    if (table->array == NULL)
-    {
-        free(table);
-        return (NULL);
-    }
+	/* Allocate memory for the array of pointers */
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 
-    /* Initialize each element in the array to NULL */
-    for (i = 0; i < size; i++)
-        table->array[i] = NULL;
+	/* Initialize each element in the array to NULL */
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
-    return (table);
+	return (table);
 }
